772f5b25ce813560b78336b615ae5010b84c4a9c
Carson Evans <carsonevans@outlook.com>
2024-03-21 12:39:34 -0400

    Solution

--- RPS.py
+++ RPS.py
@@ -1,10 +1,96 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+import itertools
+import random
 
-def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+def create_matrix(order):
+    def create_keys(order):
+        keys = ['R', 'P', 'S']
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+        for i in range((order * 2 - 1)):
+            key_len = len(keys)
+            for i in itertools.product(keys, ''.join(keys)):
+                keys.append(''.join(i))
+            keys = keys[key_len:]
+        return keys
+    
+    keys = create_keys(order)
 
-    return guess
+    matrix = {}
+    for key in keys:
+        matrix[key] = {
+            'R': {
+                'prob': 1/3,
+                'n_obs': 0
+            },
+            'P': {
+                'prob': 1/3,
+                'n_obs': 0
+            },
+            'S': {
+                'prob': 1/3,
+                'n_obs': 0
+            }
+        }
+    return matrix
+
+def update_matrix(matrix, pair, input, decay):
+    for i in matrix[pair]:
+        matrix[pair][i]['n_obs'] = decay * matrix[pair][i]['n_obs']
+    
+    matrix[pair][input]['n_obs'] = matrix[pair][input]['n_obs'] + 1
+
+    n_total = 0
+    for i in matrix[pair]:
+        n_total += matrix[pair][i]['n_obs']
+    
+    for i in matrix[pair]:
+        matrix[pair][i]['prob'] = matrix[pair][i]['n_obs'] / n_total
+
+def predict(matrix, pair):
+    probs = matrix[pair]
+
+    y = []
+    x = []
+    for k in probs:
+        for v in probs[k]:
+            if v == 'prob':
+                y.append(probs[k][v])
+                x.append(k)
+
+    if max(y) == min(y):
+        return random.choice(['R', 'P', 'S'])
+    else:
+        max_index = y.index(max(y))
+        return x[max_index]
+
+
+beat = {'R': 'P', 'P': 'S', 'S': 'R'}
+
+m = create_matrix(1)
+output = ""
+pair_diff1 = ""
+pair_diff2 = ""
+
+def player(prev_play):
+    global pair_diff1, pair_diff2, output, m
+    if not prev_play:
+        reset()
+
+    input = prev_play
+
+    pair_diff2 = pair_diff1
+    pair_diff1 = output + input
+
+    if pair_diff2 != '':
+        update_matrix(m, pair_diff2, input, 0.9)
+        output = beat[predict(m, pair_diff1)]
+    else:
+        output = random.choice(['R', 'P', 'S'])
+    
+    return output
+
+def reset():
+    global m, output, pair_diff1, pair_diff2
+    m = create_matrix(1)
+    pair_diff1 = ""
+    pair_diff2 = ""
+    output = ""
